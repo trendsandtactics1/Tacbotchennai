dde37f0826aefedf6e9970c889c28573858b5b85
trendsandtactics1 <admin@trendsandtactics.in>
2024-12-07 19:11:41 +0530

    Updated announcement-list.tsx
    remove date

--- components/admin/announcements/announcement-list.tsx
+++ components/admin/announcements/announcement-list.tsx
@@ -38,9 +38,6 @@ export function AnnouncementList({
                   Status
                 </th>
                 <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-                  Date
-                </th>
-                <th className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
                   Actions
                 </th>
               </tr>
