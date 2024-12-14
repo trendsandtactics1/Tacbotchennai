ae4e80e4b340765c34389bfb5cd525f923d70730
Boobal <a.boobalzen003@gmail.com>
2024-12-14 15:12:05 +0530

    Update message-tab.tsx

--- components/widget/tabs/message-tab.tsx
+++ components/widget/tabs/message-tab.tsx
@@ -465,7 +465,7 @@ export function MessageTab({ onTabChange }: MessageTabProps) {
                                         }
                                       ]);
                                     }}
-                                    className='px-2 py-2 text-[14px] font-semibold border border-black text-black rounded-lg hover:bg-black transition-colors'
+                                    className='px-2 py-2 text-[14px] font-semibold border border-black text-black rounded-lg hover:bg-black hover:text-white transition-colors'
                                   >
                                     Continue Here
                                   </button>
